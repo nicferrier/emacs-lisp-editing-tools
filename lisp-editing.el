51a148b01244f069f820309b7e00d05f87a15efc
Nic Ferier <nic@ferrier.me.uk>
2012-09-08 17:08:18 +0100

    add the package-load func and package-ize everything.

--- lisp-editing.el
+++ lisp-editing.el
@@ -2,8 +2,11 @@
 
 ;; Copyright (C) 2012  Nic Ferrier
 
-;; Author: Nic Ferrier(defun lisp-reinsert-as-pp () <nferrier@ferrier.me.uk>
+;; Author: Nic Ferrier  <nferrier@ferrier.me.uk>
+;; Maintainer: Nic Ferrier  <nferrier@ferrier.me.uk>
+;; Created: 18th August 2012
 ;; Keywords: lisp
+;; Version: 0.0.2
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -24,6 +27,7 @@
 
 ;;; Code:
 
+;;;###autoload
 (defun lisp-reinsert-as-pp ()
   "Read sexp at point, delete it and pretty print it back in."
   (interactive)
@@ -51,6 +55,47 @@
                (buffer-substring (point-min) (point-max)))))))
     (kill-sexp)
     (insert pp-sexp)))
+
+;;;###autoload
+(defun lisp-load-all (filename)
+  "Load all the files in a package.
+
+Takes a filename that specifies all the files in a package."
+  (interactive "Gfile list:")
+  (flet ((map-regex (buffer regex fn)
+           "Map the REGEX over the BUFFER executing FN.
+
+FN is called with the match-data of the regex.
+
+Returns the results of the FN as a list."
+           (with-current-buffer buffer
+             (save-excursion
+               (goto-char (point-min))
+               (let (res)
+                 (save-match-data
+                   (while (re-search-forward regex nil t)
+                     (let ((f (match-data)))
+                       (setq res
+                             (append res
+                                     (list
+                                      (save-match-data
+                                        (funcall fn f))))))))
+                 res)))))
+    (map-regex
+     (find-file-noselect
+      (cond
+        ((and filename
+              (file-exists-p filename)
+              (file-directory-p filename))
+         (concat (file-name-as-directory filename) "build-parts.txt"))
+        ((file-exists-p filename)
+         filename)))
+     "^\\(.*.el\\(\\.gz\\)*\\)$"
+     (lambda (md)
+       (let ((filename (match-string 0)))
+         (message "loading %s" filename)
+         (when (file-exists-p filename)
+           (load-file filename)))))))
 
 (provide 'lisp-editing)
 
